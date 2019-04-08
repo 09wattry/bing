21b0e3429fe595839bf128aeccc5a256165bc016
Ryan <09wattry@gmail.com>
2019-04-08 07:26:31 -0400

    Added async await and gpg encrypttion

--- bin.js
+++ bin.js
@@ -1,15 +1,7 @@
 const app = require("./app");
-const chrome = require('chrome-cookies-secure');
-const baseUrl = "https://www.bing.com";
-
 try {
-    chrome.getCookies(baseUrl, 'header', function (err, cookies) {
-        if (err) throw err;
-        app.setAgent("mobile");
-        app.setCookies(cookies);
-        app.setup();
-        app.run();
-    });
+    app.setup();
+    app.run();
 } catch (error) {
     console.log("An error occurred while making the request:", error);
 }
